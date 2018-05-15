850ffa296850e5e87d9319e4f209b9cc225be486
AadityaNair <aadityanair6494@gmail.com>
2018-05-15 18:45:04 +0530

    Installer and config handler

---
+++ ProjectNephos/config.py
@@ -0,0 +1,45 @@
+import os
+from configparser import ConfigParser
+from io import StringIO
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+# These values will be applied when there is no config file specified.
+default_values = {
+
+    'google': {
+        'client_secret_location': '~/client_secret.json',
+        'auth_token_location': '~/.credentials/',
+    },
+
+    'downloads': {
+        'local_save_location': '~/nephos/'
+    },
+}
+
+USER_CONFIG_LOC = '/tmp/test/'  # TODO: Fill better info at the end
+USER_CONFIG_FNAME = 'config.ini'
+config = ConfigParser()
+
+
+def parse_config() -> ConfigParser:
+    full_path = USER_CONFIG_LOC + USER_CONFIG_FNAME
+
+    if not os.path.isfile(full_path):
+        config.read_dict(default_values)
+        logger.info("Config file does not exist. Creating a new one at {}".format(USER_CONFIG_LOC))
+
+        os.makedirs(USER_CONFIG_LOC, exist_ok=True)
+        with open(full_path, 'w') as f:
+            config.write(f)
+    else:
+        config.read(full_path)
+        logger.info("Config found and read")
+        ini = StringIO()
+        config.write(ini)
+        logger.debug("Following config was read")
+        logger.debug(ini.getvalue())
+
+    return config
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+
+setup(
+        name='ProjectNephos',
+        version='0.1',
+        description='Record, Process and Upload TV Channels',
+        long_description=readme,
+        url='http://github.com/AadityaNair/ProjectNephos',
+        author='AadityaNair',
+        author_email='aadityanair6494+Nephos@gmail.com',
+        license='GPLv2',
+        packages=['ProjectNephos'],
+        install_requires=[
+            'google-api-python-client',
+        ],
+        # tests_require=[
+        #     'pytest',
+        #     'mock',
+        # ],
+        test_suite=None,  # TODO: Update later.
+        entry_points={
+            'console_scripts': ['nephos=ProjectNephos.nephos:main'],
+        },
+        zip_safe=False,
+        include_package_data=True,
+)
