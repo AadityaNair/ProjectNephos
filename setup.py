34ca37d8e2ae56eeb658c894921a54d4f86ee2c6
AadityaNair <aadityanair6494@gmail.com>
2018-05-23 11:15:16 +0530

    Directly run tests from setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
 
 with open('README.md', 'r') as f:
     readme = f.read()
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ''
+
+    def run_tests(self):
+        import shlex
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
 
 
 setup(
@@ -21,10 +37,12 @@ setup(
             'pytest',
             'mock',
         ],
-        test_suite=None,  # TODO: Update later.
+        cmdclass={'test': PyTest},
         entry_points={
             'console_scripts': ['nephos=ProjectNephos.nephos:main'],
         },
         zip_safe=False,
         include_package_data=True,
 )
+
+
