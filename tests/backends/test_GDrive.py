5168f59b1bf3d6637466c2652eefad20a1f13df2
AadityaNair <aadityanair6494@gmail.com>
2018-05-28 23:44:52 +0530

    Added some more tests.

--- tests/backends/test_GDrive.py
+++ tests/backends/test_GDrive.py
@@ -1,6 +1,9 @@
+from googleapiclient.errors import HttpError
+
 from ProjectNephos.backends.GDrive import DriveStorage
 from ProjectNephos.exceptions import OAuthFailure, FileNotFound
 from mock import MagicMock, patch
+import pytest
 
 MODULE_NAME = "ProjectNephos.backends.GDrive"
 
@@ -22,10 +25,9 @@ def test_default_object_creation(getc, Http, build):
     build.return_value = service
 
     g = DriveStorage()
+
     creds.authorize.assert_called_once_with("random1")
-
     build.assert_called_with("drive", "v3", http=authorized_http)
-
     service.files.assert_called_once()
     service.permissions.assert_called_once()
 
@@ -33,8 +35,22 @@ def test_default_object_creation(getc, Http, build):
     assert g.perm_service == "random3"
 
 
-def test_credentials_preexisting():
-    pass
+@patch(MODULE_NAME + ".Storage")
+@patch(MODULE_NAME + ".discovery.build")
+@patch(MODULE_NAME + ".Http")
+@patch(MODULE_NAME + ".DriveStorage._run_credentials_flow")
+def test_credentials_preexisting(runcf, Http, Build, Storage):
+    creds = MagicMock()
+    creds.invalid = False
+    store = MagicMock()
+    store.get.return_value = creds
+    Storage.return_value = store
+
+    DriveStorage()
+
+    Storage.assert_called_once()
+    store.get.assert_called_once()
+    runcf.assert_not_called()
 
 
 def test_credentials_flow_invalid_secret():
@@ -53,28 +69,73 @@ def test_credentials_flow_success():
     pass
 
 
-def test_exists_yes():
-    pass
+@pytest.fixture
+@patch(MODULE_NAME + ".Http")
+@patch(MODULE_NAME + ".DriveStorage._get_credentials")
+@patch(MODULE_NAME + ".discovery.build")
+def default_object(build, *args):
+    file_service = MagicMock()
+    perm_service = MagicMock()
+
+    service = MagicMock()
+    service.files.return_value = file_service
+    service.permissions.return_value = perm_service
+    build.return_value = service
+    g = DriveStorage()
+    return g, file_service, perm_service
 
 
-def test_exists_no():
-    pass
+def test_exists_yes(default_object):
+    g, file_service, perm_service = default_object
+
+    exc = MagicMock()
+    file_service.get.return_value = exc
+    ret = g.is_exists("random1")
+
+    assert ret
+    file_service.get.assert_called_once_with(fileId="random1")
+    exc.execute.assert_called_once()
+
+
+def test_exists_no(default_object):
+    g, file_service, perm_service = default_object
+
+    exc = MagicMock()
+    exc.execute.side_effect = HttpError("random2", b"random3")
+    file_service.get.return_value = exc
+    ret = g.is_exists("random1")
+
+    assert not ret
+    file_service.get.assert_called_once_with(fileId="random1")
+    exc.execute.assert_called_once()
 
 
 def test_write():
     pass
 
 
-def test_write_nonexistant():
-    pass
+@patch(MODULE_NAME + ".isfile")
+def test_write_nonexistant(isfile, default_object):
+    g, _, _ = default_object
+
+    isfile.return_value = False
+    with pytest.raises(FileNotFound):
+        g.write("random1")
+    isfile.assert_called_with("random1")
 
 
 def test_read():
     pass
 
 
-def test_read_nonexistant():
-    pass
+@patch(MODULE_NAME + ".DriveStorage.is_exists")
+def test_read_nonexistant(is_exists, default_object):
+    g, _, _ = default_object
+
+    is_exists.return_value = False
+    with pytest.raises(FileNotFound):
+        g.read("random1")
+    is_exists.assert_called_with("random1")
 
 
 def test_delete():
@@ -93,5 +154,11 @@ def test_add_perms():
     pass
 
 
-def test_add_perms_no_file():
-    pass
+@patch(MODULE_NAME + ".DriveStorage.is_exists")
+def test_add_perms_no_file(is_exists, default_object):
+    g, _, _ = default_object
+
+    is_exists.return_value = False
+    with pytest.raises(FileNotFound):
+        g.add_permissions_user("random1", "email", "role")
+    is_exists.assert_called_with("random1")
