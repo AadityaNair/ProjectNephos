801823c1991747bc4a60fbdfe0e05e2c34ae237d
AadityaNair <aadityanair6494@gmail.com>
2018-06-06 00:47:14 +0530

    Moved logger init to main file

--- ProjectNephos/__init__.py
+++ ProjectNephos/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-logging.basicConfig()
+# logging.basicConfig()
 
 # This is done to hide a ton of errors that googleapiclient randomly throws which actually
 # doesn't affect the module use at all.
