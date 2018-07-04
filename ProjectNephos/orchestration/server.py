61dbd82d24f30dae92b2171e1ca1018c647ff81a
AadityaNair <aadityanair6494@gmail.com>
2018-07-05 00:18:08 +0530

    Added recording jobs

--- ProjectNephos/orchestration/server.py
+++ ProjectNephos/orchestration/server.py
@@ -1,10 +1,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.executors.pool import ProcessPoolExecutor
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from ProjectNephos.orchestration.tasks import run_job
+from ProjectNephos.orchestration.recording import record_video
+from ProjectNephos.backends import DBStorage
 
 from logging import getLogger
 
@@ -15,7 +17,7 @@ class Server(object):
     """
     This is the main background Orchestration class. This will run every REFRESH_TIMER
     seconds and perform tasks. Each task is assumed to follow these guidelines:
-        * Available in the JOB_LIST variable.
+        * Available in the JOB_LIST variable or is an recording job.
         * Independent from any other function in the list.
         * Takes the scheduler and config as the only argument (as of now).
         * Raises anything when it fails.
@@ -32,11 +34,13 @@ class Server(object):
 
     def __init__(self, config):
         self.config = config
+        self.db = DBStorage(config)
+
         self.jobs = self.JOB_LIST
         self.running_jobs = []
         logger.debug("Starting Orchestration.")
 
-        self.sched = BackgroundScheduler(
+        self.sched = BlockingScheduler(
             jobstore=MemoryJobStore(),
             executor=ProcessPoolExecutor(5),
             job_defaults={
@@ -44,6 +48,35 @@ class Server(object):
                 "max_instances": 1,  # Total number of concurrent instances for the sam job. Change to 1 for upload.
             },
         )
+
+    def add_regular_jobs(self):
+        for item in self.jobs:
+            j = self.sched.add_job(
+                item,
+                args=[self.sched, self.config],
+                trigger="interval",
+                seconds=self.REFRESH_TIMER,
+            )
+            logger.critical("Added job {}: {}".format(j.id, j.func))
+
+    def add_recording_jobs(self):
+        job_list = self.db.get_job()
+
+        for job in job_list:
+            cron = job.start.split()
+
+            j = self.sched.add_job(
+                record_video,
+                args=[job, self.config, self.db],
+                trigger="cron",
+                minute=cron[0],
+                hour=cron[1],
+                day=cron[2],
+                month=cron[3],
+                day_of_week=cron[4],
+                year=cron[5],
+            )
+            logger.critical("Added job {}: {}".format(j.id, j.func))
 
     @staticmethod
     def endjob_listener(event):
@@ -57,11 +90,8 @@ class Server(object):
             )
 
     def run_server(self):
-        for item in self.jobs:
-            j = self.sched.add_job(
-                item, args=[self.sched, self.config], trigger="interval", seconds=self.REFRESH_TIMER
-            )
-            logger.critical("Added job {}: {}".format(j.id, j.func))
+        self.add_regular_jobs()
+        self.add_recording_jobs()
 
         self.sched.add_listener(
             self.endjob_listener, EVENT_JOB_ERROR | EVENT_JOB_EXECUTED
